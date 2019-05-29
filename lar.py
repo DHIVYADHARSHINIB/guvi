a3d9ff1987dc703bac333bc9a75fdebaedd0756b
DHIVYADHARSHINIB <50876473+DHIVYADHARSHINIB@users.noreply.github.com>
2019-05-29 21:29:46 +0530

    Update lar.py

--- lar.py
+++ lar.py
@@ -1,9 +1,12 @@
 num1=int(input(""))
 num2=int(input(""))
 num3=int(input(""))
-if(num1>num2 and num1>num3):
-  print(num1)
-elif(num2>num1 and num2>num3):
+if(num1>num2):
+  if(num2>num3):
+    print(num1)
+  else:
+    print(num2)
+elif(num2>num3):
   print(num2)
 else:
   print(num3)
